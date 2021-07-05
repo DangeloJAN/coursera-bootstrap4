209ee852b088968f5a6a06ea156d84117acdf4d3
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2021-07-05 13:14:43 -0400

    instalando varios modulos npm para ejecutar simultaneamente varios scripts y mantener vigilados varios procesos

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+
+$(document).ready(function(){
+    $("#loginInput").click(function(){
+        $("#loginModal").modal();
+    });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal();
+    });
+});
